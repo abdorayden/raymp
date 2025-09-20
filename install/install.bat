f2c7b4150be919ef7501342e042b9da19c1f92fe
abdorayden <abdenoursouane025@gmail.com>
2025-09-20 21:34:27 +0100

    add licence at the top of files

--- install/install.bat
+++ install/install.bat
@@ -1,6 +1,29 @@
 @echo off
 setlocal enabledelayedexpansion
 
+REM /************************************************************************************/
+REM /*  Copyright (c) 2025 Ray Den 							*/
+REM /*  										*/ 
+REM /*  Permission is hereby granted, free of charge, to any person obtaining a copy 	*/
+REM /*  of this software and associated documentation files (the "Software"), to deal 	*/
+REM /*  in the Software without restriction, including without limitation the rights 	*/
+REM /*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 	*/
+REM /*  copies of the Software, and to permit persons to whom the Software is 		*/
+REM /*  furnished to do so, subject to the following conditions: 			*/
+REM /*  										*/ 
+REM /*  The above copyright notice and this permission notice shall be included in 	*/
+REM /*  all copies or substantial portions of the Software. 				*/
+REM /*  										*/ 
+REM /*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 	*/
+REM /*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 	*/
+REM /*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 	*/
+REM /*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 		*/
+REM /*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 	*/
+REM /*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 	*/
+REM /*  THE SOFTWARE. 									*/
+REM /*  										*/ 
+REM /************************************************************************************/
+
 set INIT_PATH=%USERPROFILE%\.rmp\.init.lua
 set INCLUDE_PATH=-I../src/engine/lua/include
 set LIB_PATH=../src/engine/lua/lib/liblua54.a
--- main.c
+++ main.c
@@ -1,3 +1,28 @@
+/****************************************************************************************/
+/*  Copyright (c) 2025 Ray Den 								*/
+/*  											*/ 
+/*  Permission is hereby granted, free of charge, to any person obtaining a copy 	*/
+/*  of this software and associated documentation files (the "Software"), to deal 	*/
+/*  in the Software without restriction, including without limitation the rights 	*/
+/*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 		*/
+/*  copies of the Software, and to permit persons to whom the Software is 		*/
+/*  furnished to do so, subject to the following conditions: 				*/
+/*  											*/ 
+/*  The above copyright notice and this permission notice shall be included in 		*/
+/*  all copies or substantial portions of the Software. 				*/
+/*  											*/ 
+/*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 		*/
+/*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 		*/
+/*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 	*/
+/*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 		*/
+/*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 	*/
+/*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 		*/
+/*  THE SOFTWARE. 									*/
+/*  											*/ 
+/****************************************************************************************/
+
+// gcc is all you need :)
+
 #include "lua.h"
 #include "lauxlib.h"
 #include "lualib.h"
@@ -39,6 +64,9 @@ char* shift_args(int argc , char** argv)
 #endif
 
 #ifdef ONE_FILE
+
+// litteraly embed RMPManager lua code to the static buffer 
+// whatever i don't care because im gonna rewrite this engine to actual C code using lua API to access rmp framework
 static const char *const engine = 
 "local api = require(\"rmp.rmp\")\n"
 "local utils = require(\"rmp.util\")\n"
