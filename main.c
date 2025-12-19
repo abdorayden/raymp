6c81d64ac5aa894d684950e9099ce66dc5eb8210
abdorayden <abdenoursouane025@gmail.com>
2025-12-19 15:11:22 +0100

    main

--- main.c
+++ main.c
@@ -1,32 +1,31 @@
 /****************************************************************************************/
-/*  Copyright (c) 2025 Ray Den 								*/
-/*  											*/ 
-/*  Permission is hereby granted, free of charge, to any person obtaining a copy 	*/
-/*  of this software and associated documentation files (the "Software"), to deal 	*/
-/*  in the Software without restriction, including without limitation the rights 	*/
-/*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 		*/
-/*  copies of the Software, and to permit persons to whom the Software is 		*/
-/*  furnished to do so, subject to the following conditions: 				*/
-/*  											*/ 
-/*  The above copyright notice and this permission notice shall be included in 		*/
-/*  all copies or substantial portions of the Software. 				*/
-/*  											*/ 
-/*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 		*/
-/*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 		*/
-/*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 	*/
-/*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 		*/
-/*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 	*/
-/*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 		*/
-/*  THE SOFTWARE. 									*/
-/*  											*/ 
+/*  RMP - Ray's Music Player 								                            */
+/*  											                                        */
+/*  Copyright (c) 2025 Ray Den 								                            */
+/*  											                                        */
+/*  Permission is hereby granted, free of charge, to any person obtaining a copy 	    */
+/*  of this software and associated documentation files (the "Software"), to deal 	    */
+/*  in the Software without restriction, including without limitation the rights 	    */
+/*  to use, copy, modify, merge, publish, distribute, sublicense, and/or sell 		    */
+/*  copies of the Software, and to permit persons to whom the Software is 		        */
+/*  furnished to do so, subject to the following conditions: 				            */
+/*  											                                        */
+/*  The above copyright notice and this permission notice shall be included in 		    */
+/*  all copies or substantial portions of the Software. 				                */
+/*  											                                        */
+/*  THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR 		    */
+/*  IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY, 		    */
+/*  FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE 	    */
+/*  AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER 		        */
+/*  LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, 	    */
+/*  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN 		    */
+/*  THE SOFTWARE. 									                                    */
+/*  											                                        */
 /****************************************************************************************/
 
-// make audio choose multi devices headphones and more ...
-
-// gcc is all you need :)
-
-// untested and buggable code below
-// this is a new implementation of the RMP engine in C using Lua API
+// RMP Audio Engine - Multi-device support
+// Powered by Lua scripting
+// Elegant C implementation
 
 #include "lua.h"
 #include "lauxlib.h"
@@ -36,83 +35,86 @@
 #include <stdbool.h>
 #include <string.h>
 
-void print_help(char* progname)
+#define RMP_NAME "Ray's Music Player"
+#define RMP_VERSION "1.0.0"
+#define RMP_ENGINE_TAG "[RMP]"
+
+void rmp_print_usage(char* progname)
 {
+	printf("RMP - %s v%s\n", RMP_NAME, RMP_VERSION);
 	printf("Usage: %s [COMMAND] [OPTIONS]\n", progname);
-	printf("Command:\n");
-	printf("  help        		Show this help message\n");
-	/* printf("  init 		       	create plugin architecture directory , with evirement developement\n"); */
-	/* printf("  test	        	create empty window , used to test your plugin \n"); */
-	printf("Options:\n");
-	printf("  --help 	, -h    Show this help message\n");
-	printf("  --version 	, -v    Show RMP Version\n");
+	printf("\nCommands:\n");
+	printf("  help        		Display this help message\n");
+	/* printf("  init 		       	Create plugin architecture directory with development environment\n"); */
+	/* printf("  test	        	Create empty window to test your plugin\n"); */
+	printf("\nOptions:\n");
+	printf("  --help 	, -h    Display this help message\n");
+	printf("  --version 	, -v    Display RMP Version\n");
 }
 
-char* shift_args(int argc , char** argv)
+char* rmp_shift_args(int argc, char** argv)
 {
 	if(argc <= 1) {
 		return NULL;
 	}
 
-	static int index = 0;
+	static int rmp_index = 0;
 
-	if(index >= argc) {
+	if(rmp_index >= argc) {
 		return NULL;
 	}
 
-	return argv[index++];
+	return argv[rmp_index++];
 }
 
-// TODO: rewrite RMPManger engine in C
-int main(int argc , char** argv)
+int main(int argc, char** argv)
 {
+    // skip program name
+	rmp_shift_args(argc, argv);
 
-	char* program = shift_args(argc , argv);
-	if(program == NULL) {
-		// unreachable
-	}
-
-	for(int i = 0 ; i < argc ; i++){
+	for(int i = 1; i < argc; i++){
 		if(
-				strcmp(argv[i] , "--help") == 0 || 
-				strcmp(argv[i] , "-h") == 0 ||
-				strcmp(argv[i] , "/?") == 0 ||
-				strcmp(argv[i] , "-help") == 0 ||
-				strcmp(argv[i] , "/help") == 0 ||
-				strcmp(argv[i] , "help") == 0
+				strcmp(argv[i], "--help") == 0 ||
+				strcmp(argv[i], "-h") == 0 ||
+				strcmp(argv[i], "/?") == 0 ||
+				strcmp(argv[i], "-help") == 0 ||
+				strcmp(argv[i], "/help") == 0 ||
+				strcmp(argv[i], "help") == 0
 		  ){
-			print_help(argv[0]);
+			rmp_print_usage(argv[0]);
 			return 0;
 		}
 	}
 
-	char* command = shift_args(argc - 1 , argv);
-	if(command != NULL) {
-		if(strcmp(command , "help") == 0) {
-			print_help(argv[0]);
+	// Parse command from arguments
+	char* rmp_command = rmp_shift_args(argc - 1, argv);
+	if(rmp_command != NULL) {
+		if(strcmp(rmp_command, "help") == 0) {
+			rmp_print_usage(argv[0]);
 			return 0;
-		} 
-		/* else if(strcmp(command , "init") == 0) { */
-		/* 	printf("Init command called\n"); */
-		/* } else if(strcmp(command , "test") == 0) { */
-		/* 	printf("Test command called\n"); */
+		}
+		/* else if(strcmp(rmp_command, "init") == 0) { */
+		/* 	printf("RMP Init command called\n"); */
+		/* } else if(strcmp(rmp_command, "test") == 0) { */
+		/* 	printf("RMP Test command called\n"); */
 		/* } */
 	}
 
-	lua_State *L = luaL_newstate();
-	if (L == NULL) {
-		fprintf(stderr, "[RMP] failed creating Engine Lua state.\n");
+	lua_State *RMP_LUA_STATE = luaL_newstate();
+	if (RMP_LUA_STATE == NULL) {
+		fprintf(stderr, "%s Failed to create RMP Lua Engine state.\n", RMP_ENGINE_TAG);
 		return 1;
 	}
 
-	luaL_openlibs(L);
+	luaL_openlibs(RMP_LUA_STATE);
 
-	if (luaL_dostring(L , "require('rmp.RMPManager')") != LUA_OK){
-		fprintf(stderr, "[RMP] cannot execute Engine Lua code: %s\n", lua_tostring(L, -1));
-		lua_pop(L, 1);
-	    lua_close(L);
+	if (luaL_dostring(RMP_LUA_STATE, "require('rmp.RMPManager')") != LUA_OK){
+		fprintf(stderr, "%s Cannot execute RMP Engine Lua code: %s\n", RMP_ENGINE_TAG, lua_tostring(RMP_LUA_STATE, -1));
+		lua_pop(RMP_LUA_STATE, 1);
+	    lua_close(RMP_LUA_STATE);
         return 1;
 	}
-	lua_close(L);
+
+	lua_close(RMP_LUA_STATE);
 	return 0;
 }
